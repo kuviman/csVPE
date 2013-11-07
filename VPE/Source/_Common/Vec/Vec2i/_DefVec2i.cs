c8d9cdea7fde6cd113b206619d57ee041060094f
kuviman <kuviman@gmail.com>
2013-11-07 19:22:28 +0400

    Vec2i.
    Added 2d integer vector type.

---
+++ VPE/Source/_Common/Vec/Vec2i/_DefVec2i.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VitPro {
+
+	/// <summary>
+	/// Represents a 2d vector with integer coordinates.
+	/// </summary>
+	[Serializable]
+	public struct Vec2i {
+		public readonly int X, Y;
+		public Vec2i(int x, int y) {
+			X = x;
+			Y = y;
+		}
+
+		public override string ToString() {
+			return string.Format("({0}; {1})", X, Y);
+		}
+
+		public static explicit operator Vec2i(Vec2 v) {
+			return new Vec2i((int)v.X, (int)v.Y);
+		}
+		public static implicit operator Vec2(Vec2i v) {
+			return new Vec2(v.X, v.Y);
+		}
+	}
+
+}
