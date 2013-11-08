7cf17d1e72c8996bd8e873007ba33a97f660e0f2
kuviman <kuviman@gmail.com>
2013-11-08 23:53:48 +0400

    Int uniform.
    Added int uniforms.

---
+++ VPE/Source/Engine/Graphics/Shader/Int.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace VitPro.Engine {
+
+    partial class Shader {
+
+        struct UniformInt : IUniform {
+            public int X;
+            public void Set(int loc, ref int textures) {
+                GL.Uniform1(loc, X);
+            }
+        }
+
+        /// <summary>
+        /// Set a uniform int variable.
+        /// </summary>
+        /// <param name="name">Variable name.</param>
+        public void SetInt(string name, int x) {
+            uniforms[Loc(name)] = new UniformInt { X = x };
+        }
+
+    }
+
+}
