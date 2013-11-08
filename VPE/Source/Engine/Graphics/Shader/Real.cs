e3483a32e6a4292cf61140bcb357833809a45cb7
kuviman <kuviman@gmail.com>
2013-11-08 23:55:19 +0400

    Real uniform.
    Added real (double / float) uniforms.

---
+++ VPE/Source/Engine/Graphics/Shader/Real.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenTK.Graphics.OpenGL;
+
+namespace VitPro.Engine {
+
+    partial class Shader {
+
+        struct UniformDouble : IUniform {
+            public double X;
+            public void Set(int loc, ref int textures) {
+                GL.Uniform1(loc, (float)X);
+            }
+        }
+
+        /// <summary>
+        /// Set a uniform float variable.
+        /// </summary>
+        /// <param name="name">Variable name.</param>
+        public void SetReal(string name, double x) {
+            uniforms[Loc(name)] = new UniformDouble { X = x };
+        }
+
+    }
+
+}
