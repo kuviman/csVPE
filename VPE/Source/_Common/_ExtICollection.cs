59403b3a40eaa1dd839d3d3d93b9d84d01960458
kuviman <kuviman@gmail.com>
2013-12-09 03:01:34 +0400

    ICollection.AddRange.

---
+++ VPE/Source/_Common/_ExtICollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VitPro {
+
+	public static class ExtICollection {
+
+		public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> range) {
+			foreach (var o in range)
+				collection.Add(o);
+		}
+
+	}
+
+}
